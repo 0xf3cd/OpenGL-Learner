4b291b4a870d68302e85da66c42defac253e44dd
0xf3cd <nq.maigre@gmail.com>
2022-04-22 01:43:17 -0700

    Created shaders and program

--- WebGL/starter/src/App.tsx
+++ WebGL/starter/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
-import vertexShader from './shaders/vertex.glsl';
-import fragmentShader from './shaders/fragment.glsl';
+import vertexShaderSrc from './shaders/vertex.glsl';
+import fragmentShaderSrc from './shaders/fragment.glsl';
 
 // Ref: https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Getting_started_with_WebGL
 
@@ -9,27 +9,78 @@ const CANVAS_CONFIG = {
   height: 750,
   id: 'glCanvas',
   rgba: {
-    r: 0.18,
-    g: 0.35,
-    b: 0.15,
+    r: 0.125,
+    g: 0.25,
+    b: 0.1,
     a: 1,
   },
 };
 
-const main = (): void => {
-  const canvas = document.getElementById(CANVAS_CONFIG.id) as HTMLCanvasElement;
-
+const createGlContext = (canvasId: string): WebGL2RenderingContext | null => {
   // Initialize the GL context
-  const gl = canvas.getContext('webgl2');
-
-  // Only continue if WebGL is available and working
-  if (gl === null) {
-    alert('Unable to initialize WebGL. Your browser or machine may not support it.');
-    return;
-  } else {
-    console.log('WebGL object created.');
-    console.log(gl);
+  const canvas = document.getElementById(canvasId) as HTMLCanvasElement;
+  if (!canvas) {
+    console.error('Could not find the canvas element.');
+    return null;
   }
+  const gl = canvas.getContext('webgl2');
+  if (!gl) {
+    console.error('Unable to initialize WebGL. Your browser or machine may not support it.');
+    return null;
+  }
+  return gl;
+};
+
+const loadShader = (gl: WebGL2RenderingContext, type: number, source: string): WebGLShader | null => {
+  // Create the shader.
+  const shader = gl.createShader(type);
+  if (!shader) {
+    console.error('Could not create shader.');
+    return null;
+  }
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  // Check the compile result.
+  const status = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (!status) {
+    const info = gl.getShaderInfoLog(shader);
+    gl.deleteShader(shader);
+    console.error(`An error occurred when compiling the shader:\n${info}`);
+    return null;
+  }
+
+  return shader; // Safely return the shader!
+};
+
+const createProgram = (gl: WebGL2RenderingContext, vertexShader: WebGLShader, fragmentShader: WebGLShader): WebGLProgram | null => {
+  const program = gl.createProgram();
+  if (!program) {
+    console.error('Could not create the program.');
+    return null;
+  }
+
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  const status = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (!status) {
+    const info = gl.getProgramInfoLog(program);
+    gl.deleteProgram(program);
+    console.error(`An error occurred when linking the program:\n${info}`);
+    return null;
+  }
+
+  return program;
+};
+
+const main = (): void => {
+  // Create the GL context.
+  const gl = createGlContext(CANVAS_CONFIG.id);
+  if (!gl) { return; }
+  console.log('WebGL object created.');
+  console.log(gl);
 
   // Set clear color, fully opaque
   const rgba = CANVAS_CONFIG.rgba;
@@ -37,8 +88,18 @@ const main = (): void => {
   // Clear the color buffer with specified clear color
   gl.clear(gl.COLOR_BUFFER_BIT);
 
+  // Create the shaders.
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertexShaderSrc);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSrc);
+  if (!vertexShader || !fragmentShader) { return; }
+
+  // Create the program.
+  const shaderProgram = createProgram(gl, vertexShader, fragmentShader);
+  if (!shaderProgram) { return; }
+
   console.log(vertexShader);
   console.log(fragmentShader);
+  console.log(shaderProgram);
 };
 
 export const App = (): JSX.Element => {
